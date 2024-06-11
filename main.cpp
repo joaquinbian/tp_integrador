98cd0d2b28d6efc39597df56203b81ca015861b2
joaquinbian <joacobian21@gmail.com>
2024-06-10 22:21:08 -0300

    ultima version

--- main.cpp
+++ main.cpp
@@ -4,56 +4,60 @@
 #include <random>
 #include <cstdlib>
 #include <ctime>
- 
+
 using namespace std;
- 
+
 int repartirCarta();
 int asignarPalo();
 int asignarEmbaucadora();
- 
+string Ganador;
+int PuntosGanador;
+int PuntosA = 0, PuntosB = 0;
+
 int main (){
     setlocale(LC_ALL, "spanish");
- 
+
     int opcion;
     char confirmacion, salir;
     string jugador1, jugador2;
     int puntajeJugador1, puntajeJugador2, ronda1Jugador1, ronda2Jugador1, ronda3Jugador1, ronda1Jugador2, ronda2Jugador2, ronda3Jugador2;
- 
+
     while(true){
- 
+
         do{
-            cout << "EMBAUCADO" << endl << "------------------" << endl;
+            cout << "EMBAUCADO" << endl << "---------------------" << endl;
             cout << " 1 - JUGAR" << endl;
             cout << " 2 - ESTADISTICAS" << endl;
-            cout << " 3 - CREDITOS" << endl << endl << "------------------" << endl << endl;
-            cout << " 0 - SALIR" << endl << endl << "------------------" << endl << endl;
- 
+            cout << " 3 - CREDITOS" << endl << "---------------------" << endl;
+            cout << " 0 - SALIR" << endl << "---------------------" << endl;
+
             cout << "ELIJA UNA OPCIÓN: ";
             cin >> opcion;
             system ("cls");
- 
+
         }
         while(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 0 && opcion != 1992);
- 
+
         switch(opcion)
         {
- 
+
         case 1:
             system ("cls") ;
             cout << "EMBAUCADO" << endl << "------------------------------------------------------------------------" << endl;
-            cout << "ANTES DE COMENZAR, DEBEN REGISTRAR SUS NOMBRES:" << endl;
+            cout << "Antes de comenzar, deben registrar sus nombres:" << endl;
             cout <<endl;
-            cout << "NOMBRE DEL JUGADOR 1? ";
+            cout << "Nombre del jugador 1? ";
             cin >> jugador1;
-            cout << "NOMBRE DEL JUGADOR 2? ";
+            cout << "Nombre del jugador 2? ";
             cin >> jugador2;
- 
+
             cout << endl << "Confirmar nombres? (S/N) ";
             cin >>   confirmacion;
- 
+
+
             while(confirmacion == 'N' || confirmacion == 'n')
             {
- 
+
                 system ("cls") ;
                 cout << "EMBAUCADO" << endl << "------------------" << endl;
                 cout << "ANTES DE COMENZAR, DEBEN REGISTRAR SUS NOMBRES:" << endl;
@@ -66,26 +70,29 @@ int main (){
                 cin >> confirmacion;
             }
             system ("cls") ;
- 
-            for(int x = 0; x < 3; x ++)
-            {
-                cout << "EMBAUCADO" << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+
+            int puntosronda[3];
+            int puntosrondab[3];
+            for(int x = 0; x < 3; x ++){
+
+                cout << "EMBAUCADO" << endl << "----------------------------------------" << endl;
                 cout << "RONDA #" << x + 1  <<endl;
-                cout << jugador1 << " Vs " <<jugador2 << endl;
- 
+                cout << jugador1 << " Vs " << jugador2 << endl<<endl;
+
                 char * num[5] = {"10", "J", "Q", "K", "A"};
                 char * palo[4] = {"Trebol", "Pica", "Corazon", "Diamante"};
                 int valores[5] = {10, 11, 12, 15, 20};
                 int i, C1 = 0, C2 = 0, C3 = 0, C4 = 0, C5 = 0, C6 = 0, C7 = 0, C8 = 0, C9 = 0, C10 = 0;
- 
+
                 srand(time(0)); //semilla o seed
                 int z = asignarEmbaucadora();
-                cout << jugador1 << endl;
-                cout << endl;
+                cout << "+------------------------------+"<< endl;
+                cout << "|                              |"<< endl;
+                cout << "|  " << jugador1 << " (PUNTOS " << PuntosA << " )" << endl;
                 for(i=0;i<5;i++){
                   int x = repartirCarta();
                   int y = asignarPalo();
- 
+
                   if(C1 == 0 && y!=z){
                     C1 = valores[x];
                   }else if(C2 == 0 && y!=z){
@@ -97,11 +104,10 @@ int main (){
                   }else if(C5 == 0 && y!=z){
                     C5 = valores[x];
                   }
-                  cout << num[x] << " de " << palo[y] << endl;
+                  cout << "|     " << num[x] << " de " << palo[y] << endl;
                 }
-                cout << endl;
-                cout << jugador2 << endl;
-                cout << endl;
+                cout << "|                              |" << endl;
+                cout << "|  " << jugador2 << " (PUNTOS " << PuntosB << " )" << endl;
                 for(i=0;i<5;i++){
                   int x = repartirCarta();
                   int y = asignarPalo();
@@ -116,40 +122,58 @@ int main (){
                   }else if(C10 == 0 && y!=z){
                     C10 = valores[x];
                   }
-                  cout << num[x] << " de " << palo[y] << endl;
+                  cout << "|     " << num[x] << " de " << palo[y] << endl;
                 }
+                    cout << "|                              |" << endl;
+                    cout << "|   Embaucadora: " << palo[z] << endl;
+                    cout << "|                              |" << endl;
+                    cout << "+------------------------------+"<<endl;
                     cout << endl;
-                    cout << "Embaucadora: " << palo[z] << endl;
-                    cout << endl;
+
+                    cout << "Puntajes Obtenidos:" << endl;
+                    cout << "----------------------------------------" << endl;
                     int totalPuntosA = C1 + C2 + C3 + C4 + C5;
-                    cout << jugador1 << ": " << C1 << " + " << C2 << " + " << C3 << " + " << C4 << " + " << C5 << " = " << totalPuntosA << " Puntos";
+                    cout << jugador1 << " = " << totalPuntosA << " Puntos";
                     cout << endl;
                     int totalPuntosB = C6 + C7 + C8 + C9 + C10;
-                    cout << jugador2 << ": " << C6 << " + " << C7 << " + " << C8 << " + " << C9 << " + " << C10 << " = " << totalPuntosB << " Puntos";
+                    cout << jugador2 << " = " << totalPuntosB << " Puntos";
                     cout << endl;
- 
-                /*string palos[4] = {"Corazon", "Pica", "Trebol", "Diamante"};
- 
-                // Usar el tiempo actual como semilla para obtener diferentes resultados en cada ejecución
-                unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-                std::mt19937 gen(seed); // Generador de números aleatorios Mersenne Twister
-                std::uniform_int_distribution<> distribucion(0, 3); // Rango de 0 a 4 (índices del arreglo)
- 
-                // Generar un índice aleatorio
-                int indice_aleatorio = distribucion(gen);
- 
-                // Elegir un valor aleatorio del arreglo
-                string valor_aleatorio = palos[indice_aleatorio];
- 
-                // Imprimir el valor aleatorio seleccionado
-                std::cout << "Embaucadora: " << valor_aleatorio << std::endl;*/
- 
- 
+                    cout << endl;
+                    puntosronda[x] = totalPuntosA;
+                    puntosrondab[x] = totalPuntosB;
+                    PuntosA += totalPuntosA;
+                    PuntosB += totalPuntosB;
+
                 system ("pause");
                 system ("cls") ;
             }
- 
- 
+
+
+            cout << "EMBAUCADO" << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+            cout << endl;
+            cout << "RONDA     " << jugador1 << "     " << jugador2;
+            cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+            cout << "  1        " << puntosronda[0] << "      " << puntosrondab[0] << endl;
+            cout << "  2        " << puntosronda[1] << "      " << puntosrondab[1] << endl;
+            cout << "  3        " << puntosronda[2] << "      " << puntosrondab[2];
+            cout << endl << "------------------------------------------------------------------------------------------------------------------------" << endl;
+            cout << "TOTAL     " << PuntosA << "     " << PuntosB << endl;
+            cout << endl;
+            if(PuntosA>PuntosB){
+              string Ganador = jugador1;
+              int PuntosGanador = PuntosA;
+            } else {
+              Ganador = jugador2;
+              PuntosGanador = PuntosB;
+            }
+            cout << "GANADOR: " << Ganador << " con " << PuntosGanador << " puntos de victoria." << endl;
+            cout << endl;
+            PuntosA = 0;
+            PuntosB = 0;
+
+            system ("pause");
+            system ("cls") ;
+
             break;
         case 2:
             system ("cls") ;
@@ -165,7 +189,7 @@ int main (){
             cout << "MARQUEZ LEONEL" << endl << "Número de legajo: 31037" ;
             cout << endl << endl;
             cout << "Correspondientes al grupo Número 3.";
- 
+
             cout << endl << endl;
             system ("pause");
             system ("cls") ;
@@ -191,7 +215,7 @@ int main (){
         }
     }
 }
- 
+
 int repartirCarta(){
   int mano;
   mano = rand() % 5;
